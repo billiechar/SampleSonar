f12af07d81e3ad5a6d8f9dcf844aa1d4cc8bf05c
nwestbury <nick.westbury@verint.com>
2017-12-07 13:13:10 +0000

    first commit

---
+++ sampleSonar/SampleClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sampleSonar
+{
+    public class SampleClass
+    {
+        public string sampleMethod()
+        {
+            if(true) { throw new Exception(); }
+        }
+    }
+}
